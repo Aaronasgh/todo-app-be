283869c04fb7b68ae7e67923983ca8f370a63b70
Aaronasgh <aaron.asgharzadeh@gmail.com>
2025-06-24 23:37:58 +0200

    Sending get response to frontend

--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/todos", (req, res) => {
+  res.json([{ Text: "Hello World!" }]);
 });
 
 app.listen(port, () => {
