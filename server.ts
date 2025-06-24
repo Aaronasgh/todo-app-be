d2b45f3d51dc0ed104c2b2cc11df9b71e93fde1a
Aaronasgh <aaron.asgharzadeh@gmail.com>
2025-06-25 00:14:59 +0200

    Send proper JSON response

--- server.ts
+++ server.ts
@@ -5,8 +5,20 @@ const app = express();
 app.use(cors());
 const port = 4000;
 
+const todos = [
+  { id: "1", text: "Buy Milk" },
+  { id: "2", text: "Do Laundry" },
+  { id: "3", text: "Clean Room" },
+  { id: "4", text: "Workout" },
+  { id: "5", text: "Go Swimming" },
+  { id: "6", text: "Do Homework" },
+  { id: "7", text: "Call Nick" },
+  { id: "8", text: "Water Plants" },
+  { id: "9", text: "Study" },
+];
+
 app.get("/todos", (req, res) => {
-  res.json([{ Text: "Hello World!" }]);
+  res.json(todos);
 });
 
 app.listen(port, () => {
